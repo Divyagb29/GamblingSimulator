1dc7ba946a481f0584e5a28f87448314a1a72936
divyagb29 <divya2229mgb@gmail.com>
2020-08-23 16:17:22 +0530

    finding gambler got 50% win/lose

--- GamblingSimulator.sh
+++ GamblingSimulator.sh
@@ -3,9 +3,12 @@
 echo "Welcome to Gambler Simulator"
 STAKE=100
 BET=1
-if [ $((RANDOM%2)) -eq '0' ]
+while [ $STAKE -lt '100' &&  $STAKE -gt '50' ] 
+do
+if [ $((RANDOM%2)) -eq '0'  ]
 then
 	STAKE=$((STAKE-BET))
 else
 	STAKE=$((STAKE+BET))
 fi
+done
